b226d38a2f3b1e8ebfd2c6fa94aa7bcc501052da
rishabhraghav19 <rishabhraghav8521@gmail.com>
2026-05-27 11:09:12 +0530

    exp 7

---
+++ App.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", padding: "20px" }}>
+      <h1>Student Details</h1>
+
+      <div style={{
+        border: "1px solid black",
+        width: "250px",
+        margin: "10px auto",
+        padding: "10px",
+        borderRadius: "8px"
+      }}>
+        <p>Name: Rishabh raghav</p>
+        <p>Roll No: 2025B01010321</p>
+        <p>Subject: dsa</p>
+      </div>
+
+      <div style={{
+        border: "1px solid black",
+        width: "250px",
+        margin: "10px auto",
+        padding: "10px",
+        borderRadius: "8px"
+      }}>
+        <p>Name: Pandey</p>
+        <p>Roll No: 1186</p>
+        <p>Subject: Cyber</p>
+      </div>
+
+      <div style={{
+        border: "1px solid black",
+        width: "250px",
+        margin: "10px auto",
+        padding: "10px",
+        borderRadius: "8px"
+      }}>
+        <p>Name: Shubh chaudhary</p>
+        <p>Roll No: 2503201001132</p>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
