e1c477b1aebfd9ae12bea6ef3610eb00a6f14b04
rishabhraghav19 <rishabhraghav8521@gmail.com>
2026-05-27 11:10:13 +0530

    exp 8

--- App.jsx
+++ App.jsx
@@ -1,47 +1,26 @@
-import React from "react";
-
-function App() {
-  return (
-    <div style={{ textAlign: "center", padding: "20px" }}>
-      <h1>Student Details</h1>
-
-      <div style={{
-        border: "1px solid black",
-        width: "250px",
-        margin: "10px auto",
-        padding: "10px",
-        borderRadius: "8px"
-      }}>
-        <p>Name: Rishabh raghav</p>
-        <p>Roll No: 2025B01010321</p>
-        <p>Subject: dsa</p>
-      </div>
-
-      <div style={{
-        border: "1px solid black",
-        width: "250px",
-        margin: "10px auto",
-        padding: "10px",
-        borderRadius: "8px"
-      }}>
-        <p>Name: Pandey</p>
-        <p>Roll No: 1186</p>
-        <p>Subject: Cyber</p>
-      </div>
-
-      <div style={{
-        border: "1px solid black",
-        width: "250px",
-        margin: "10px auto",
-        padding: "10px",
-        borderRadius: "8px"
-      }}>
-        <p>Name: Shubh chaudhary</p>
-        <p>Roll No: 2503201001132</p>
-      </div>
-
-    </div>
-  );
+import "./App.css";
+import React,{useState} from "react";
+function App(){
+    const[count, setCount] = useState(0);
+    const increment =() => {
+        setCount(count +1);
+    };
+    const decrement =()=> {
+        setCount(count-1);
+    };
+    const reset =()=>{  
+        setCount(0);
+    };
+    return(
+        <div className="container">
+            <h1>React counter Application</h1>
+            <h2 className="counter">{count}</h2>
+            <div className="buttons">
+                <button onClick={increment}>Increment(+)</button>
+                <button onClick={decrement}>Decrement (-)</button>
+                <button onClick ={reset}>Reset</button>
+            </div>
+        </div>
+    );
 }
-
 export default App;
